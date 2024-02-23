23d7c34b20480e8900b5d22202b2a55967efd0bb
aochoa21 <146290813+aochoa21@users.noreply.github.com>
2024-02-23 02:30:54 +0000

    Done Lab 4

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18]
     print(sub2)
     print(sub1)
+    merged_str = sub1 + sub2
     print(merged_str)
 
     #########################################
